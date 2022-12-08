49162fe54902b3259e8d34f2f0e06f72c5e3cdcf
cadangan99 <onnelikrio@gmail.com>
2022-12-08 10:36:55 +0700

    Update function Sum di dalam File Sum.js
    \n\n Menambahkan fitur jika salah satu argument nya = 0 maka akan di loop dan di jumlahkan untuk mendapatkan total penjumlahan\n\nUpdate file Sum.js

--- sum.js
+++ sum.js
@@ -1,1 +1,33 @@
-function sum(a, b) {a + b};
+function Sum(a, b) { 
+    var c;
+
+    if(a == 0 || b == 0)
+    {
+        if(a == 0)
+        {
+            c = SumLooping(b);
+        }
+        else
+        {
+            c = SumLooping(a);
+        }
+    }
+    else
+    {
+        c = a + b;
+    }
+
+    return c;
+};
+
+function SumLooping(n) {
+    var total = 0;
+
+    for (let i = 1; i <= n; i++) {
+        total +=i;     
+    }
+
+    return total;
+}
+
+console.log(Sum(0, 9));
